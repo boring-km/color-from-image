8a286c607e47ab911be2c13e7a0e017ef455fa18
boring-km <kms0644804@naver.com>
2022-04-04 17:45:01 +0900

    테스트 코드 수정

--- test/data/get_divisors_test.dart
+++ test/data/get_divisors_test.dart
@@ -1,18 +1,14 @@
-import 'package:color_picker/core/logger.dart';
+import 'package:color_picker/data/get_divisors.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('3024에서 1과 자기 자신을 제외한 약수 구하기', () {
     const num = 3024;
-    const mid = num / 2;
-    final result = <int>[];
-    for (var i = 3; i <= mid; i++) {
-      if (num % i == 0) {
-        result.add(i);
-      }
-    }
-    equals(result ==
-        [3, 4, 6, 7, 8, 9, 12, 14, 16, 18, 21, 24, 27, 28, 36, 42, 48, 54, 56, 63, 72, 84,
-          108, 112, 126, 144, 168, 189, 216, 252, 336, 378, 432, 504, 756, 1008, 1512]);
+
+    final actualResult = GetDivisors.by(num);
+
+    var expected = [2, 3, 5, 6, 7, 8, 11, 13, 15, 17, 20, 23, 26, 27, 35, 41, 47, 53, 55, 62, 71, 83,
+      107, 111, 125, 143, 167, 188, 215, 251, 335, 377, 431, 503, 755, 1007, 1511];
+    expect(actualResult, expected);
   });
 }
